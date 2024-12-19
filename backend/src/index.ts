9d2e7162bf4974e83adc315f2fe25a6777ecf46c
Bilal Ali <bilalaliofficial1@gmail.com>
2024-12-19 10:27:07 +0530

    Added typescript and a basic server

---
+++ backend/src/index.ts
@@ -0,0 +1,18 @@
+import express, {Request, Response} from "express";
+import cors from "cors";
+import "dotenv/config";
+
+
+const app= express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/test", async(req: Request, res: Response)=>{
+    res.json({
+        message:"Hello"
+    })
+})
+
+app.listen(3000, ()=>{
+    console.log("server started at 3000");
+})
