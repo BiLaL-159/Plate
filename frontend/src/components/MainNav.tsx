046dfc47749a54f956f55da47f9f76d495fb3b50
Bilal Ali <bilalaliofficial1@gmail.com>
2025-01-20 08:34:52 +0530

    Added authenticated link

--- frontend/src/components/MainNav.tsx
+++ frontend/src/components/MainNav.tsx
@@ -1,14 +1,18 @@
 import { useAuth0 } from "@auth0/auth0-react"
 import { Button } from "./ui/button"
+import UsernameMenu from "./UsernameMenu";
 export default function MainNav() {
-    const {loginWithRedirect}=useAuth0();
+    const {loginWithRedirect, isAuthenticated}=useAuth0();
   return (
-    <Button 
+    <span className="flex space-x-2 items-center">
+        {isAuthenticated? <UsernameMenu/> :  <Button 
      variant="ghost" 
      className="font-bold text-black"
      onClick={async ()=> await loginWithRedirect()}
      >
     Login
-    </Button>
+    </Button>}
+    </span>
+   
   )
 }
